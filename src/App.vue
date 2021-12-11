78c9edd03f70e847d8f568afef564f134adfbf8f
BehrangHaghdoust <62517402+BehrangHaghdoust@users.noreply.github.com>
2021-12-11 21:39:49 +0330

    Navbar by vue...

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/profile">Profile</router-link>
-  </div>
+
+<nav class="navbar navbar-expand-lg navbar-light mb-5" style="background-color: #e3f2fd;" id="nav">
+      <router-link class="navbar-brand" to="/">NorthDev</router-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor03">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <router-link class="nav-link" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/about">About</router-link>
+        </li>
+        <li class="nav-item">
+              <router-link class="nav-link" to="/profile">Profile</router-link>
+
+        </li>
+
+      </ul>
+      <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
   <router-view class="container"/>
 </template>
 
@@ -16,16 +37,20 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
 
 #nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
+.navbar-brand{
+  color: #2c3e50 !important;
+}
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #239dcd;
+}
+
+.navbar-brand{
+
 }
 </style>
