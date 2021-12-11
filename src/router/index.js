032c8c97ce766059b93ca11d2c86b18f56709366
BehrangHaghdoust <62517402+BehrangHaghdoust@users.noreply.github.com>
2021-12-11 20:59:00 +0330

    make profile menu btn

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Profile from '../views/Profile.vue'
 
 const routes = [
   {
@@ -13,6 +14,12 @@ const routes = [
     name: 'About',
     component: About
   },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
+  },
+
 
 ]
 
---
+++ src/views/Profile.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="Profile">
+      <h1>This is a Profile page </h1>
+  </div>
+</template>
