7f852ff963cecb58083c64f702d2924f1d0bcd14
BehrangHaghdoust <62517402+BehrangHaghdoust@users.noreply.github.com>
2021-12-12 11:47:36 +0330

    LOGIN form without validation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam sunt
+      laboriosam quia mollitia voluptatum quisquam corporis, eveniet officia
+      aliquid quod, maiores adipisci modi obcaecati ut quas praesentium hic,
+      earum voluptatibus?
+    <hr>
+    </div>
+     <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam sunt
+      laboriosam quia mollitia voluptatum quisquam corporis, eveniet officia
+      aliquid quod, maiores adipisci modi obcaecati ut quas praesentium hic,
+      earum voluptatibus?
+    <hr>
+    </div>
+     <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam sunt
+      laboriosam quia mollitia voluptatum quisquam corporis, eveniet officia
+      aliquid quod, maiores adipisci modi obcaecati ut quas praesentium hic,
+      earum voluptatibus?
+    <hr>
+    </div>
+     <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam sunt
+      laboriosam quia mollitia voluptatum quisquam corporis, eveniet officia
+      aliquid quod, maiores adipisci modi obcaecati ut quas praesentium hic,
+      earum voluptatibus?
+    <hr>
+    </div>
+     <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam sunt
+      laboriosam quia mollitia voluptatum quisquam corporis, eveniet officia
+      aliquid quod, maiores adipisci modi obcaecati ut quas praesentium hic,
+      earum voluptatibus?
+    <hr>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+
+};
 </script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,23 @@
 <template>
-    <div class="Login">
-<button class="btn btn-success" @click="doLogin">Login</button>
+  <div class="Login">
+    <div class="row">
+      <div class="col-lg-4 mx-auto border shadow rounded p-4">
+        <h1 class="text-center mt-3 mb-4">Login</h1>
+        <form @submit.prevent="doLogin">
+          <div class="form-group">
+            <label for="usernameInput">Username</label>
+            <input type="text" class="form-control" id="usernameInput" v-model="username"/>
+          </div>
+          <div class="form-group">
+            <label for="passwordInput">Password</label>
+            <input type="password" class="form-control" id="passwordInput" v-model="password" />
+          </div>
+
+          <button type="submit" class="btn btn-primary mt-3">Login</button>
+        </form>
+      </div>
     </div>
+  </div>
 </template>
 
 
@@ -9,11 +25,23 @@
 
 export default {
   name: 'Login',
+  data(){
+      return{
+          username : '',
+          password : ''
+      }
+  },
 
   methods :{
       doLogin(){
-          this.$store.commit("login" , "abc1453")
-          this.$router.push("/Profile")
+        //   this.$store.commit("login" , "abc1453")
+        //   this.$router.push("/Profile")
+        console.log(
+            this.username
+        )
+           console.log(
+            this.password
+        )
       }
   }
 
