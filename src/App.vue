9521880f7655823c1b3c4a42e480cdef3efb9677
BehrangHaghdoust <62517402+BehrangHaghdoust@users.noreply.github.com>
2021-12-12 09:31:48 +0330

    make navbar as a component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
 
-<nav class="navbar navbar-expand-lg navbar-light mb-5" style="background-color: #e3f2fd;" id="nav">
-      <router-link class="navbar-brand" to="/">NorthDev</router-link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarColor03">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <router-link class="nav-link" to="/">Home</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/about">About</router-link>
-        </li>
-        <li class="nav-item">
-              <router-link class="nav-link" to="/profile">Profile</router-link>
-
-        </li>
-
-      </ul>
-      <form class="form-inline">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
-      </form>
-    </div>
-  </nav>
+<Navbar/>
   <router-view class="container"/>
 </template>
+
+
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+
+
+
+
 
 <style>
 #app {
@@ -50,7 +46,10 @@
   color: #239dcd;
 }
 
-.navbar-brand{
+/* .navbar-brand{
 
-}
+} */
 </style>
+
+
+
---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="Navbar22">
+<nav class="navbar navbar-expand-lg navbar-light mb-5" style="background-color: #e3f2fd;" id="nav">
+      <router-link class="navbar-brand" to="/">NorthDev</router-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor03">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <router-link class="nav-link" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/about">About</router-link>
+        </li>
+        <li class="nav-item">
+              <router-link class="nav-link" to="/profile">Profile</router-link>
+
+        </li>
+
+      </ul>
+      <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Navbar'
+
+}
+</script>
