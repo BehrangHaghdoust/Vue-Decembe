e39bf1110c6506d898edec50b1a44d89b7dc743d
BehrangHaghdoust <62517402+BehrangHaghdoust@users.noreply.github.com>
2021-12-13 11:50:15 +0330

    Edit article

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -6,6 +6,44 @@
       <h3>{{ article.title }}</h3>
       <div>{{ article.content }}</div>
     </article>
+    <div v-if="$store.state.isAuthenticated">
+      <hr />
+      <button class="btn btn-warning mr-3" @click="edit = !edit">Edit</button>
+      <button class="btn btn-danger">Remove</button>
+      <hr />
+      <form @submit.prevent="doEdit" class="pb-5" v-if="edit">
+        <div class="form-group">
+          <label for="TitleInput">Title</label>
+          <input
+            type="text"
+            class="form-control"
+            id="TitleInput"
+            v-model="title"
+          />
+        </div>
+
+        <div class="form-group">
+          <label for="DescriptionInput">Description</label>
+          <textarea
+            class="form-control"
+            id="DescriptionInput"
+            rows="3"
+            v-model="description"
+          ></textarea>
+        </div>
+        <div class="form-group">
+          <label for="ContentInput">Content</label>
+          <textarea
+            class="form-control"
+            id="ContentInput"
+            rows="9"
+            v-model="content"
+          ></textarea>
+        </div>
+
+        <button class="btn btn-warning">Edit article</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -15,13 +53,35 @@ export default {
   data() {
     let articles = localStorage.getItem("articles");
     articles = JSON.parse(articles);
-    let article =articles.find(
-        article => article.slug == this.$route.params.slug
-    )
+    let article = articles.find(
+      (article) => article.slug == this.$route.params.slug
+    );
     return {
       articles: articles,
-      article:article
+      article: article,
+      title: article.title,
+      description: article.description,
+      content: article.content,
+      edit: false,
     };
+  },
+  methods: {
+    doEdit() {
+      let index = this.articles.findIndex(
+        (article) => article.slug == this.$route.params.slug
+      );
+      this.articles[index] = {
+        title: this.title,
+        slug: this.title.replaceAll(" ", "-").toLowerCase(),
+        description: this.description,
+        content: this.content,
+      }
+       let database = JSON.stringify(this.articles)
+         localStorage.setItem('articles', database)
+         this.article = this.articles[index]
+         this.edit = false
+         this.$router.push(`/article/${this.articles[index].slug}`)
+    },
   },
 };
 </script>
