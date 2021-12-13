51b0fcce25392e23f78f253803c6f94d8795ed1b
BehrangHaghdoust <62517402+BehrangHaghdoust@users.noreply.github.com>
2021-12-13 09:10:18 +0330

    new part----Make article

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,7 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam sunt
-      laboriosam quia mollitia voluptatum quisquam corporis, eveniet officia
-      aliquid quod, maiores adipisci modi obcaecati ut quas praesentium hic,
-      earum voluptatibus?
-    <hr>
-    </div>
-     <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam sunt
-      laboriosam quia mollitia voluptatum quisquam corporis, eveniet officia
-      aliquid quod, maiores adipisci modi obcaecati ut quas praesentium hic,
-      earum voluptatibus?
-    <hr>
-    </div>
-     <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam sunt
-      laboriosam quia mollitia voluptatum quisquam corporis, eveniet officia
-      aliquid quod, maiores adipisci modi obcaecati ut quas praesentium hic,
-      earum voluptatibus?
-    <hr>
-    </div>
-     <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam sunt
-      laboriosam quia mollitia voluptatum quisquam corporis, eveniet officia
-      aliquid quod, maiores adipisci modi obcaecati ut quas praesentium hic,
-      earum voluptatibus?
-    <hr>
-    </div>
-     <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam sunt
-      laboriosam quia mollitia voluptatum quisquam corporis, eveniet officia
-      aliquid quod, maiores adipisci modi obcaecati ut quas praesentium hic,
-      earum voluptatibus?
-    <hr>
-    </div>
+    
   </div>
 </template>
 
